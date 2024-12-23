d9b35a65a2a1c66a08803628e7739e9c14c102b7
linedj <eunjun998@gmail.com>
2024-12-23 15:51:44 +0900

    등록 ë번호 증가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,19 +12,26 @@ class App{
         Scanner sc = new Scanner(System.in);
         System.out.println("==명언 앱==");
 
+        int lastno = 1;
+
         while(true){
             System.out.print("명령) ");
             String command = sc.nextLine();
+
             if(command.equals("종료")){
                 System.out.println(("명언 앱을 종료합니다"));
                 break;
             }
+
             else if(command.equals("등록")){
                 System.out.print("명언 : ");
                 sc.nextLine();
 
                 System.out.print("작가 : ");
                 sc.nextLine();
+
+                System.out.println(lastno + "번 명언이 등록되었습니다.");
+                lastno++;
             }
 
         }
