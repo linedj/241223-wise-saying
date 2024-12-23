87680934be84cf6a858fdc6ab6fbd6f1008b6c95
linedj <eunjun998@gmail.com>
2024-12-23 15:21:09 +0900

    work3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         App app = new App();
@@ -7,7 +9,16 @@ public class Main {
 
 class App{
     public void run(){
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("==명언 앱==");
+
+        System.out.print("명언 : ");
+        sc.nextLine();
+        System.out.print("작가 : ");
+        sc.nextLine();
         System.out.println("명령) 종료");
+
+        sc.close();
     }
 }
